5081d787e51202583d0ff6a9b5766f7a7122970a
Marko Kosyk <kosyk.markian@gmail.com>
2023-07-22 22:49:04 +0300

    modal window added project start

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const buttonsModals = document.querySelectorAll('.show-modal');
+const modalWindow = document.querySelector('.modal.hidden');
+console.log(modalWindow);
+
+
+buttonsModals.forEach(element => {
+  element.addEventListener('click', () => {
+    modalWindow.classList.remove('hidden');
+    console.log(modalWindow, '2');
+  })
+});

575edbd0cc616a1c2270f147e1637c3cd18092c0
Marko Kosyk <kosyk.markian@gmail.com>
2023-07-22 20:54:51 +0300

    score and highscore implemented

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,22 +1,41 @@
 'use strict';
 
 //Implementing the logic of creating guessed number
-const guessNumber = Math.round(Math.random() * 20);
+const highScoreGet = localStorage.getItem('highScore');
+const highScoreGetNumber = Number(highScoreGet);
 
+let guessNumber = Math.round(Math.random() * 20);
+let scoreField = document.querySelector('.score');
+let score = 20;
+let highScoreField = document.querySelector('.highscore');
+highScoreField.innerText = !highScoreGet ? 0 : highScoreGet;
+let highScore;
 
 //Implementing the logic of getting input data and check if it is higher, lower or exact as guessed earlier
 
 const checkResult = (number, message) => {
 
   if (guessNumber > number) {
-    message.innerText = 'Try again your number should be higher'
+    score--
+    message.innerText = 'Try again your number should be higher';
+    scoreField.innerText = score;
+    return
   }
   else if (guessNumber < number) {
-    message.innerText = 'Try again your number is too high'
+    score--
+    message.innerText = 'Try again your number is too high';
+    scoreField.innerText = score;
+    return
   }
   else {
-    message.innerText = `Congratulations you won this game the ${number}`
+    message.innerText = `Congratulations you won this game`
     document.body.style.backgroundColor = 'green';
+    document.querySelector('.number').innerText = number;
+    if (!highScoreGetNumber || score > highScoreGetNumber) {
+      highScore = score;
+      localStorage.setItem('highScore', highScore);
+      highScoreField.innerText = highScore;
+    }
   }
 }
 
@@ -29,8 +48,13 @@ const submit = () => {
 }
 
 const again = () => {
+  score = 20
   const messageParagrpaph = document.querySelector('.message');
+  document.querySelector('.guess').value = 0
   messageParagrpaph.innerText = 'Start guessing...';
   document.body.style.backgroundColor = '#222';
+  guessNumber = Math.round(Math.random() * 20);
+  document.querySelector('.number').innerText = '?';
+  scoreField.innerText = score;
 }
 

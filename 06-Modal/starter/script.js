0784d80aa73bfbea8f079496ffff732d2e03f638
Marko Kosyk <kosyk.markian@gmail.com>
2023-07-23 23:13:44 +0300

    modal windows added

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,12 +2,21 @@
 
 const buttonsModals = document.querySelectorAll('.show-modal');
 const modalWindow = document.querySelector('.modal.hidden');
+let overlay = document.querySelector('.overlay.hidden');
+let btnClose = document.querySelector('.close-modal');
 console.log(modalWindow);
+
+
+const Close = () => {
+  overlay.classList.add('hidden');
+  modalWindow.classList.add('hidden');
+}
 
 
 buttonsModals.forEach(element => {
   element.addEventListener('click', () => {
     modalWindow.classList.remove('hidden');
-    console.log(modalWindow, '2');
+    overlay.classList.remove('hidden');
   })
 });
+

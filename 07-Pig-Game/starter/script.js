37c2302c68cdd555f031006a376df783332d689f
Marko Kosyk <kosyk.markian@gmail.com>
2023-07-26 23:07:29 +0300

    switching players function

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,7 +3,8 @@
 
 
 // Start with putting to zero all elements before any game
-let guessNumber, generalScorePlayer1, currentFieldScore, sectionPlayer, players;
+let guessNumber, generalScorePlayer1, currentFieldScore, sectionPlayer, players, j;
+j = 0;
 sectionPlayer = document.querySelector('.player--active');
 const scorePlayers = document.querySelectorAll('.score');
 const currentScores = document.querySelectorAll('.current-score');
@@ -11,19 +12,25 @@ const currentScores = document.querySelectorAll('.current-score');
 
 const SwitchPlayer = (activePlayer) => {
   players = document.querySelectorAll('.player');
-  activePlayer.classList.add('player--active');
   for (let i = 0; i < players.length; i++) {
     if (players[i] === sectionPlayer) {
       if (players[i + 1] === undefined) {
         i = 0
         sectionPlayer.classList.remove('player--active');
         players[i].classList.add('player--active');
+        break;
       }
       else {
         sectionPlayer.classList.remove('player--active')
         players[i + 1].classList.add('player--active');
       }
     }
+    sectionPlayer = document.querySelector('.player--active');
+    if (i === 0 && j === 0) {
+      j++
+      break;
+    }
+
   }
 }
 

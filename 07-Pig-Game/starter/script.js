bdd7b84e26bc306aaf63f3c961a9f8e243e8a34b
Marko Kosyk <kosyk.markian@gmail.com>
2023-07-26 19:26:50 +0300

    remove classname

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,8 +3,29 @@
 
 
 // Start with putting to zero all elements before any game
+let guessNumber, generalScorePlayer1, currentFieldScore, sectionPlayer, players;
+sectionPlayer = document.querySelector('.player--active');
 const scorePlayers = document.querySelectorAll('.score');
 const currentScores = document.querySelectorAll('.current-score');
+
+
+const SwitchPlayer = (activePlayer) => {
+  players = document.querySelectorAll('.player');
+  activePlayer.classList.add('player--active');
+  for (let i = 0; i < players.length; i++) {
+    if (players[i] === sectionPlayer) {
+      if (players[i + 1] === undefined) {
+        i = 0
+        sectionPlayer.classList.remove('player--active');
+        players[i].classList.add('player--active');
+      }
+      else {
+        sectionPlayer.classList.remove('player--active')
+        players[i + 1].classList.add('player--active');
+      }
+    }
+  }
+}
 
 const Start = () => {
   for (let i = 0; i < scorePlayers.length; i++) {
@@ -14,36 +35,11 @@ const Start = () => {
 }
 
 
-
-let guessNumber, generalScorePlayer1, currentFieldScore;
-
-const NewGame = () => {
-  Start()
-}
-
-const SwitchPlayer = (activePlayer) => {
-  let players = document.querySelectorAll('.player');
-  for (let i = 0; i < players.length; i++) {
-    if (players[i] === activePlayer) {
-      if (players[i + 1] === undefined) {
-        i = 0
-        activePlayer.classList.remove('player--active');
-        players[i].classList.add('player--active');
-      }
-      else {
-        players[i].classList.remove('player--active')
-        players[i + 1].classList.add('player--active');
-      }
-    }
-  }
-}
-
 const RollDice = () => {
   guessNumber = Math.round(Math.random() * 5) + 1;
   const cubes = document.querySelector('.dice');
   cubes.src = `dice-${guessNumber}.png`;
   cubes.style.display = 'block';
-  let sectionPlayer = document.querySelector('.player--active');
   if (sectionPlayer) {
     currentFieldScore = document.querySelector('.current-score');
     if (guessNumber === 1) {
@@ -55,7 +51,10 @@ const RollDice = () => {
   }
 }
 
-
+const NewGame = () => {
+  Start();
+  SwitchPlayer(sectionPlayer);
+}
 
 
 
